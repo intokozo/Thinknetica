6138dc17dbb9c8de306cced2a25b97888fe96af9
intokozo <morozbar@bk.ru>
2019-01-22 20:50:15 +0400

    Lesson 1

---
+++ Lession 1/Quadratic_equation.rb
@@ -0,0 +1,18 @@
+puts "Давайте решим квадратное уравнение.\nВведите \"a\""
+a = gets.chomp.to_i
+puts "Введите \"b\""
+b = gets.chomp.to_i
+puts "Введите \"с\""
+c = gets.chomp.to_i
+d = b**2 - 4 * a * c
+if d < 0
+  puts "Уравнение решений не имеет, так как дискриминант меньше нуля #{d}"
+elsif d == 0
+  x = -b/(2*a)
+  puts "Один корень, x =#{x}, дискриминант равен #{d}"
+elsif d > 0
+  x_one = (-b + Math.sqrt(d))/(2*a)
+  x_two = (-b - Math.sqrt(d))/(2*a)
+  puts "Два корня, x1 =#{x_one} x2 =#{x_two}, дискриминант равен #{d}"
+else puts "Такого не может быть"
+end
