3f287019f177e521f05d7dea76c5c2248a7c50dd
intokozo <morozbar@bk.ru>
2019-01-23 12:58:33 +0400

    Lession_1.1

--- Lession_1/Quadratic_equation.rb
+++ Lession_1/Quadratic_equation.rb
@@ -1,18 +1,27 @@
 puts "Давайте решим квадратное уравнение.\nВведите \"a\""
-a = gets.chomp.to_i
+  a = gets.chomp.to_i
+
 puts "Введите \"b\""
-b = gets.chomp.to_i
+  b = gets.chomp.to_i
+
 puts "Введите \"с\""
-c = gets.chomp.to_i
+  c = gets.chomp.to_i
+
 d = b**2 - 4 * a * c
+
 if d < 0
-  puts "Уравнение решений не имеет, так как дискриминант меньше нуля #{d}"
-elsif d == 0
-  x = -b/(2*a)
-  puts "Один корень, x =#{x}, дискриминант равен #{d}"
-elsif d > 0
-  x_one = (-b + Math.sqrt(d))/(2*a)
-  x_two = (-b - Math.sqrt(d))/(2*a)
-  puts "Два корня, x1 =#{x_one} x2 =#{x_two}, дискриминант равен #{d}"
-else puts "Такого не может быть"
+      puts "Уравнение решений не имеет, так как дискриминант меньше нуля #{d}"
+
+  elsif d == 0
+    x = -b / (2 * a)
+      puts "Один корень, x =#{x}, дискриминант равен #{d}"
+
+  elsif d > 0
+  f = Math.sqrt(d)
+    x_one = (-b + f) / (2 * a)
+    x_two = (-b - f) / (2 * a)
+      puts "Два корня, x1 =#{x_one} x2 =#{x_two}, дискриминант равен #{d}"
+
+  else puts "Такого не может быть"
+
 end
--- Lession_1/ideal_weight.rb
+++ Lession_1/ideal_weight.rb
@@ -1,6 +1,9 @@
 puts "Как вас зовут?"
-name = gets.chomp.capitalize
+  name = gets.chomp.capitalize
+
 puts "Какой у вас рост?"
-height = gets.chomp.to_i
+  height = gets.chomp.to_i
+
 ideal_weight = height - 110
+
 puts ideal_weight > 0 ? "#{name}, ваш идеальный вес #{ideal_weight}" : "#{name} у вас идеальный вес!"
